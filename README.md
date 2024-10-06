48eaccd9186aa4c89a9f9786ef8aa2f2f3efbe9b
d3viliz3d <d3viliz3d@users.noreply.github.com>
2024-10-06 21:23:16 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Curriculum Vitae
+
+Backend Software Engineer
---
+++ src/metadata/metadata.js
@@ -0,0 +1,124 @@
+module.exports = {
+  name: 'Cristian Guerra',
+  title: 'Backend Software Engeneer',
+  facts: {
+    'Location': '<a href="https://maps.app.goo.gl/MzpKutdJTeo3nc2e8"><i class="fa fa-home fact-icon"></i>Bangkok, Thailand</a>',
+    'LinkedIn': `<a href="https://www.linkedin.com/in/d3viliz3d">
+                  <i class="fa-brands fa-linkedin fact-icon"></i>d3viliz3d
+                </a>`,
+    'Github': `<a href="https://github.com/d3viliz3d"><i class="fa-brands fa-github fact-icon"></i>d3viliz3d</a>`,
+    'StackOverflow': `<a href="https://stackoverflow.com/users/1846089/d3viliz3d"><i class="fa-brands fa-stack-overflow fact-icon"></i>d3viliz3d</a>`,
+    'Email': '<a href="mailto:d3viliz3d@gmail.com"><i class="fa fa-envelope fact-icon"></i>d3viliz3d@gmail.com</a>'
+  },
+  intro: `
+  <p>I am Backend Developer with over 8 years of experience in designing and implementing scalable, secure, and high-performance backend systems, specializing in Python-based frameworks and cloud infrastructure.<br/>
+  Additionally, I have several years of expertise in system administration and network management.</p>
+  <p>After taking a sabbatical year to explore the world, immerse myself in different cultures, and broaden my perspectives, I am ready to resume my career and take on new challenges.</p>
+  `,
+  languages: [
+    ['Italian', 100],
+    ['English', 100],
+    ['Thai', 60],
+    ['Spanish', 40],
+    ['French', 20],
+    ['Japanese', 20],
+  ],
+  skills: [
+    {
+      type: "Backend development",
+      description: 'Python (Django, Flask, DRF, PyTest), PHP (Laravel), C# (ASP.NET Core), PostgreSQL, MySQL.',
+    },
+    {
+      type: "Frontend experience",
+      description: 'React, Angular, jQuery, SCSS/SASS.',
+    },
+    {
+      type: "DevOps",
+      description: 'AWS, CI/CD pipelines, Test-Driven Development, CloudWatch logs management and analysis, AmazonMQ, Terraform.',
+    },
+    {
+      type: "Plugin Development and SEO",
+      description: 'Developed custom plugins and optimized websites for WordPress and Magento, with a focus on performance and SEO.',
+    },
+    {
+      type: "Various",
+      description: 'Github, Azure DevOps, Sentry, Adobe Suite, Video streaming, Cryptography, Virtualization.',
+    }
+  ],
+  positions: [
+    {
+      company: "DIGITHAI Software Group Co., Ltd.",
+      title: 'Backend Developer & Tech Lead',
+      period: 'Aug 2018 - Gen 2024',
+      skills: ['Django', 'Flask', 'Laravel', 'C#', 'PostgreSQL', 'MySQL', 'Wordpress', 'CI/CD', 'AWS', 'OAuth'],
+      contents: `
+<ul>
+  <li>Worked as an external contractor for the <strong>World Food Programme (part of the United Nations)</strong>, contributing to several high-impact projects. The most notable was <a href="https://executiveboard.wfp.org/document_download/WFP-0000001575" target="_blank">WFP SCOPE</a>, a platform that supports refugee assistance programs worldwide, benefiting around 50 million people.
+    <ul>
+      <li>As part of the backend development team, I was responsible for the <strong>registration and management of beneficiaries</strong>, as well as the printing and distribution of <strong>SCOPECARDs</strong>, which are NFC-enabled cards allowing beneficiaries to purchase essential goods like food and sanitary products.</li>
+      <li>Given the platform’s large user base, code quality, performance, and data privacy were paramount. Mistakes in the system could have serious repercussions, making <strong>security</strong> and <strong>scalability</strong> critical priorities.</li>
+      <li>Implemented <strong>OAuth integration</strong> across the entire portal, managing the migration of existing users to OAuth servers and facilitating the registration of new users.</li>
+      <li>Integrated <strong>REST APIs</strong> with external services such as <strong>Western Union</strong>, SMS providers, and other UN systems, often working under <strong>tight deadlines</strong> with incomplete requirements.</li>
+    </ul>
+  </li>
+
+  <li>Contributed to other key projects, including:
+    <ul>
+      <li><a href="https://partnerconnect.wfp.org/" target="_blank">WFP Partner Connect</a>: a platform used to evaluate and approve partner proposals for relief operations in country offices worldwide. I managed a backend system and handled its integration with <strong>DocuSign</strong>.</li>
+      <li><a href="https://gems.wfp.org/" target="_blank">WFP GEMS</a>: a global asset management and inventory system. I upgraded outdated dependencies, resolved conflicts and deprecations, while also adding new features and integrating it with WFP’s ERP system (<strong>WINGS</strong>).</li>
+    </ul>
+  </li>
+
+  <li>Promoted to <strong>Tech Lead</strong>, where I supervised a team of three developers. My responsibilities included task delegation, resolving bottlenecks, conducting code reviews, and in general ensuring the smooth execution of operations.</li>
+
+  <li>Contributed to the development of a <strong>portal for tour and hotel management</strong>, which included handling multi-currency pricing, multi-language support, room bookings, voucher generation, and more.</li>
+
+  <li>Developed websites and portals for <strong>Bayer Pharmaceuticals</strong>, smaller businesses, and restaurants. I also provided IT technical support to the <strong>Italian Embassy in Bangkok</strong>.</li>
+</ul>
+
+<p><em>References available upon request.</em></p>
+`
+    },
+    {
+      company: "YOKKAO Boxing Co., Ltd.",
+      title: 'Full-Stack Developer',
+      period: 'Jun 2016 - Jul 2018',
+      skills: ['Magento', 'Wordpress', 'MySQL'],
+      contents: `
+<ul>
+  <li>Managed the complete <strong>front-end and back-end development</strong> for multiple websites, including the company’s primary <strong>Magento-based e-commerce platform</strong>.
+    <ul>
+      <li>Responsibilities included managing <strong>product listings</strong> across multiple languages, <strong>inventory management</strong>, <strong>customer databases</strong>, and <strong>admin accounts</strong>.</li>
+      <li>Developed and implemented <strong>custom plugins</strong> and executed <strong>complex database queries</strong> to enhance functionality, fix bugs, and integrate new features.</li>
+      <li>Integrated various payment gateways, including <strong>credit cards</strong>, <strong>PayPal</strong>, and <strong>cryptocurrency</strong> options. Focused heavily on <strong>security testing</strong> and addressing vulnerabilities.</li>
+      <li>Led <strong>SEO efforts</strong> and conducted <strong>social media marketing</strong> campaigns through newsletters and social media platforms.</li>
+    </ul>
+  </li>
+
+  <li>The company also organized <strong>international Muay Thai events</strong>, and I managed <strong>video editing</strong> for the fights and handled <strong>live streaming</strong> to audiences of up to 1,000 viewers.</li>
+</ul>
+`
+    },
+    {
+      company: "Bonlex Europe Srl",
+      title: 'IT System Administrator',
+      period: 'Oct 2008 - Oct 2015',
+      skills: ['Windows Server', 'Linux Server', 'Network management', 'Network Security', 'ERP', 'MySQL', 'PHP', 'Wordpress'],
+      contents: `
+<ul>
+  <li>Oversaw the company’s entire <strong>network infrastructure</strong>, including the management of both <strong>Windows and Linux servers</strong>, mobile devices, and security systems.
+    <ul>
+      <li>Responsibilities spanned <strong>hardware and software maintenance</strong>, ensuring <strong>data security</strong>, and implementing robust <strong>backup and disaster recovery</strong> plans.</li>
+    </ul>
+  </li>
+
+  <li>Designed and developed the company website, following both <strong>technical and design aspects</strong>.</li>
+
+  <li>Acted as the primary point of contact for <strong>ERP system support</strong>, assisting users with their issues, and writing custom database queries to resolve them when necessary.</li>
+</ul>
+`
+    },
+  ],
+  education: '<strong>ITIS J. F. Kennedy</strong> Diploma in Information Technlogies',
+  interests: 'I\'m passionate about technology beyond work. I enjoy DJing and recently began experimenting with electronic music production.<br/>I follow and invest in the crypto markets, delving into the projects and their technical aspects.<br/>I have trained in Muay Thai for 8 years, competing occasionally, which has helped me develop discipline, focus, and perseverance.<br/>My love for travel and photography has taken me to various landscapes around the world. I edit my photos using Photoshop and Lightroom, publishing them online.'
+};
